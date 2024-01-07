afe258ea9478d6a78309fc08fc24d087dd09dbcb
KubraIsik <kuisik@gmail.com>
2024-01-07 21:55:01 +0300

    upload files

---
+++ tobeto_testCases/constants/globalConstants.py
@@ -0,0 +1,15 @@
+# login
+USER_MAIL = "icimdekisi@gmail.com"
+USER_PASSWORD = "1234567_"
+
+# test_addExperience.py constants
+USER_PROFILE_BUTTON_XPATH = "//*[@id='__next']/div/nav/div[1]/div/div/div[2]"
+USER_PROFILE_INFO_BUTTON_XPATH ="//*[@id='__next']/div/nav/div[1]/div/div/div[2]/ul/li[1]/a"
+PAGE_TITLE_XPATH = "//*[@id='__next']/div/main/section/div/div/div[1]/div/a[1]/span[2]"
+USER_EXPERINCES_PAGE_LINK_XPATH = "//*[@id='__next']/div/main/section/div/div/div[1]/div/a[2] "
+CORPORATION_NAME_INPUT_XPATH = "//div[@id='__next']/div/main/section/div/div/div[2]/form/div/div/input"
+POSITION_INPUT_XPATH = "//div[@id=\'__next\']/div/main/section/div/div/div[2]/form/div/div[2]/input"
+SECTOR_INPUT_XPATH = "//div[@id=\'__next\']/div/main/section/div/div/div[2]/form/div/div[3]/input"
+COUNTRY_DROPDOWN_XPATH = "//div[@id=\'__next\']/div/main/section/div/div/div[2]/form/div/div[4]/select"
+START_CALENDAR_XPATH="//div[@id=\'__next\']/div/main/section/div/div/div[2]/form/div/div[5]/div/div/input"
+FINISH_CALENDAR_XPATH= "//*[@id='__next']/div/main/section/div/div/div[2]/form/div/div[6]/div/div/input"
